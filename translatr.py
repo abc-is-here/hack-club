f347ccd954f1f5a7d39376862f01b06ac67efabb
abc <abhaygupta.hsj@gmail.com>
2024-07-01 18:44:32 +0530

    The final stretch is over

--- translatr.py
+++ translatr.py
@@ -3,8 +3,12 @@ from tkinter import ttk
 from googletrans import Translator as trs
 from googletrans import LANGUAGES as lng
 
+color1 = '#020f12'
+color2 = '#05d7ff'
+color3 = '#65e7ff'
+color4 = 'BLACK'
+
 def conversion(text="type", src = "english", dest = "hindi"):
-    text1 = text
     src1 = src
     dest1 = dest
     trans = trs()
@@ -22,37 +26,49 @@ def data():
 
 root = Tk()
 root.title("Translator")
-root.geometry("500x800")
-root.config(bg = 'light blue')
+root.geometry("500x680")
+root.config(bg = 'black')
 
-lab_txt = Label(root, text = "Translator", font = ("times roman new", 40, "bold"), bg='light blue', fg='white')
-lab_txt.place(x = 100, y = 40, height=50, width=300)
+img = PhotoImage(file = 'assets/logo.png')
+root.iconphoto(False, img)
+
+lab_txt = Label(root, text = "Translator", font = ("Comic Sans MS", 40, "bold"), bg = 'black', fg=color2)
+lab_txt.place(x = 100, y = 25, height=50, width=300)
 
 frame = Frame(root).pack(side = BOTTOM)
 
-lab_txt = Label(root, text = "Source text", font = ("times roman new", 20, "bold"), bg='light blue', fg='grey')
-lab_txt.place(x = 100, y = 100, height=20, width=300)
+lab_txt = Label(root, text = "Source text", font = ("Comic Sans MS", 20, "bold"), bg = 'black', fg='WHITE')
+lab_txt.place(x = 100, y = 90, height=25, width=300)
 
-src_txt = Text(frame, font = ("times roman new", 30, "bold"), wrap=WORD)
+src_txt = Text(frame, font = ("Comic Sans MS", 30, "bold"), wrap=WORD, bg='GREY', fg='WHITE')
 src_txt.place(x = 10, y = 130, height=200, width=480)
 
 list_txt = list(lng.values())
 
-com_src = ttk.Combobox(frame, values = list_txt)
+com_src = ttk.Combobox(frame, values = list_txt, font = ("Comic Sans MS", 15, "bold"))
 com_src.place(x = 10, y = 350, height=40, width=150)
 com_src.set("English")
 
-button_change = Button(frame, text = "Translate", relief=RAISED, command=data)
-button_change.place(x = 170, y = 350, height=40, width=150)
+button_change = Button(frame,text = "changes to",relief=RAISED,command=data,font = ("Comic Sans MS", 20, "bold"),background=color2,
+    foreground=color4,
+    activebackground=color3,
+    activeforeground=color1,
+    highlightthickness=2,
+    highlightbackground=color2,
+    highlightcolor='WHITE',
+    border=0,
+    cursor="hand1",
+    )
+button_change.place(x = 170, y = 350, height=40, width=160)
 
-com_dest = ttk.Combobox(frame, values = list_txt)
+com_dest = ttk.Combobox(frame, values = list_txt, font = ("Comic Sans MS", 15, "bold"))
 com_dest.place(x = 340, y = 350, height=40, width=150)
 com_dest.set("Hindi")
 
-lab_txt = Label(root, text = "Destination text", font = ("times roman new", 20, "bold"), bg='light blue', fg='grey')
-lab_txt.place(x = 100, y = 410, height=20, width=300)
+lab_txt = Label(root, text = "Destination text", font = ("Comic Sans MS", 20, "bold"), bg = 'black', fg="WHITE")
+lab_txt.place(x = 100, y = 410, height=25, width=300)
 
-dest_txt = Text(frame, font = ("times roman new", 30, "bold"), wrap=WORD)
+dest_txt = Text(frame, font = ("Comic Sans MS", 30, "bold"), wrap=WORD, bg='GREY', fg='WHITE')
 dest_txt.place(x = 10, y = 450, height=200, width=480)
 
 root.mainloop()
